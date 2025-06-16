d0124eabfa297e662622b756e7c4f70a276b44bf
Jeong <jeongm0220@gmail.com>
2025-06-16 16:01:49 +0900

    refactor(auth): make nickname optional in LoginResponseDto

--- schemas/auth.py
+++ schemas/auth.py
@@ -1,6 +1,8 @@
 # Pydantic 모듈은 데이터 유효성 검사, 데이터 변환, 모델 시퀀싱 등을 지원하는 모듈
 # Python 클래스 BaseModel을 상속받은 클래스로 이 클래스는 입력값을 검증하고, 자동을 타입 변환도 해줌
 from pydantic import BaseModel, EmailStr
+from typing import Optional
+
 
 class LoginRequestDto(BaseModel):
     email: EmailStr
@@ -12,6 +14,6 @@ class LoginResponseDto(BaseModel):
     access_token: str
     refresh_token: str
     member_seq: int
-    nickname: str
+    nickname: Optional[str] = None
     email: str
 
